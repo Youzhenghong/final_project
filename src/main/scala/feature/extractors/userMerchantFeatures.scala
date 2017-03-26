3a5f5791a262bb6cffc7429c6567bad22f63ae5e
youzhenghong <yzh19950927@gmail.com>
2017-03-26 17:28:05 +0800

    userMerchantFeature extraction complete

--- src/main/scala/feature/extractors/userMerchantFeatures.scala
+++ src/main/scala/feature/extractors/userMerchantFeatures.scala
@@ -6,7 +6,7 @@ import org.apache.spark.sql.DataFrame
   * Created by youzhenghong on 21/03/2017.
   */
 object userMerchantFeatures {
-  def userMerchant(df : DataFrame) : Unit = {
+  def userMerchant(df : DataFrame) : DataFrame = {
     val userMerchantAction = userMerchantActionCount(df)
     userMerchantAction.cache()
 
@@ -16,7 +16,7 @@ object userMerchantFeatures {
         .withColumnRenamed("count", "click_action")
     }
 
-    val purchase_acction = {
+    val purchase_action = {
       userMerchantAction.filter("action_type = 2")
         .select("user_id", "merchant_id", "count")
         .withColumnRenamed("count", "purchase_action")
@@ -28,9 +28,14 @@ object userMerchantFeatures {
         .withColumnRenamed("count", "favor_action")
     }
 
+    val user_merchant = click_action.join(favor_action, Seq("user_id", "merchant_id"))
+        .join(purchase_action, Seq("user_id", "merchant_id"))
+
+    user_merchant.show()
     //click_action.show()
     //purchase_acction.show()
     //favor_action.show()
+    user_merchant
 
   }
 
